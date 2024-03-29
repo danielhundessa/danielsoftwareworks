284a4aba49f6a2249c3bad7045b1563f9407ad91
danielhundessa <161267282+danielhundessa@users.noreply.github.com>
2024-03-29 01:48:14 -0400

    Add files via upload

---
+++ backend/src/main/java/com/thewebsite/danielsoftwareworks/DanielsoftwareworksApplication.java
@@ -0,0 +1,13 @@
+package com.thewebsite.danielsoftwareworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanielsoftwareworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanielsoftwareworksApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thewebsite/danielsoftwareworks/DanielsoftwareworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thewebsite.danielsoftwareworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanielsoftwareworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
